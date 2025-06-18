a4f42666c8e824683624c0efd48c0847e741dbba
Santiagovs7 <villadasantasantiago208@gmail.com>
2025-06-18 18:15:14 -0500

    ya se llaman a los coevaluadores al registrar ya esta integrado con pdf y excel

--- src/main/java/com/juan/curso/springboot/webapp/saep/repository/UsuariosRepository.java
+++ src/main/java/com/juan/curso/springboot/webapp/saep/repository/UsuariosRepository.java
@@ -1,7 +1,13 @@
 package com.juan.curso.springboot.webapp.saep.repository;
 
+import com.juan.curso.springboot.webapp.saep.model.Rol;
 import com.juan.curso.springboot.webapp.saep.model.Usuarios;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface UsuariosRepository extends JpaRepository<Usuarios,Long> {
+    List<Usuarios> findByRol(Rol rol);
 }
